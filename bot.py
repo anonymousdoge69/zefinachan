c826713241219146c5f9f59b73a2f226a17518ba
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-28 22:06:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ bot.start(bot_token=TOKEN)
 async def get_proxies(e):
  mode = e.text[1:]
  r = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol={}&timeout=10000&country=all&ssl=all&anonymity=all".format(mode))
- with io.BytesIO(r.content.encode()) as file:
+ with io.BytesIO(r.content) as file:
       file.name = "{}_proxies.txt".format(mode)
       await e.respond(file=file)
 
@@ -37,26 +37,26 @@ async def _start(e):
 By using our service you agree to our terms of use!
 • AutoUpdate each 69 Mins
 • Support: @roseloverx_support
-""", buttons=[Button.text("HTTP"), Button.text("SOCKS4"), Button.text("SOCKS5")])
+""", buttons=[Button.text("HTTP", resize=True), Button.text("SOCKS4", resize=True), Button.text("SOCKS5", resize=True)])
 
 @bot.on(events.NewMessage(pattern="^HTTP", func=lambda e: e.is_private))
 async def http__(e):
  r = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all")
- with io.BytesIO(r.content.encode()) as file:
+ with io.BytesIO(r.content) as file:
       file.name = "http_proxies.txt"
       await e.respond(file=file)
 
 @bot.on(events.NewMessage(pattern="^SOCKS4", func=lambda e: e.is_private))
 async def socks4__(e):
  r = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all&ssl=all&anonymity=all")
- with io.BytesIO(r.content.encode()) as file:
+ with io.BytesIO(r.content) as file:
       file.name = "socks4_proxies.txt"
       await e.respond(file=file)
 
 @bot.on(events.NewMessage(pattern="^SOCKS5", func=lambda e: e.is_private))
 async def socks5__(e):
  r = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all")
- with io.BytesIO(r.content.encode()) as file:
+ with io.BytesIO(r.content) as file:
       file.name = "socks5_proxies.txt"
       await e.respond(file=file)
 
